8cc6b78f8e0d9947c1a999ece42b9df6563585c5
Marlon Baker <e525850@ds.cao.co.la.ca.us>
2023-07-26 15:00:53 -0700

    adding newdata.js

---
+++ src/frontend/newdata.js
@@ -0,0 +1,308 @@
+[
+    {
+    "_id": "648d789225e153e0de0e84f7",
+    "title": "High Blossom Shirt",
+    "des": "designer shirt for 2023",
+    "img": "https://ecommercemcm.s3.us-west-1.amazonaws.com/other/marbaker.swe_black_young_man_wearing_asian_desiner_shirt_high_f_338b3137-c744-4f1b-b7dc-99963feb5b5f+copy2.png",
+    "categories": [
+    "shirt",
+    "men"
+    ],
+    "size": [
+    "S",
+    "M"
+    ],
+    "color": [
+    "black"
+    ],
+    "price": 30,
+    "inStock": true,
+    "createdAt": "2023-06-17T09:10:42.494Z",
+    "updatedAt": "2023-06-17T09:10:42.494Z",
+    "__v": 0
+    },
+    {
+    "_id": "648d79ee25e153e0de0e84fb",
+    "title": "Mystical Flower Chef Shirt",
+    "des": "designer chef shirt for 2023",
+    "img": "https://ecommercemcm.s3.us-west-1.amazonaws.com/other/marbaker.swe_black_young_man_wearing_asian_desiner_shirt_high_f_338b3137-c744-4f1b-b7dc-99963feb5b5f+copy3.png",
+    "categories": [
+    "shirt",
+    "men"
+    ],
+    "size": [
+    "S",
+    "M",
+    "L",
+    "XL"
+    ],
+    "color": [
+    "black",
+    "white",
+    "yellow",
+    "green"
+    ],
+    "price": 32,
+    "inStock": true,
+    "createdAt": "2023-06-17T09:16:30.497Z",
+    "updatedAt": "2023-06-17T09:16:30.497Z",
+    "__v": 0
+    },
+    {
+    "_id": "648d7a9725e153e0de0e84fd",
+    "title": "Roman Golden Armor Button Shirt",
+    "des": "designer shirt for 2022",
+    "img": "https://ecommercemcm.s3.us-west-1.amazonaws.com/other/marbaker.swe_black_young_man_wearing_asian_desiner_shirt_high_f_338b3137-c744-4f1b-b7dc-99963feb5b5f+copy4.png",
+    "categories": [
+    "shirt",
+    "men"
+    ],
+    "size": [
+    "S",
+    "M",
+    "L",
+    "XL"
+    ],
+    "color": [
+    "black",
+    "white",
+    "yellow",
+    "blue"
+    ],
+    "price": 35,
+    "inStock": true,
+    "createdAt": "2023-06-17T09:19:19.205Z",
+    "updatedAt": "2023-06-17T09:19:19.205Z",
+    "__v": 0
+    },
+    {
+    "_id": "648d7af525e153e0de0e84ff",
+    "title": "Hokkaido Temple Long Shirt",
+    "des": "designer shirt for 2020",
+    "img": "https://ecommercemcm.s3.us-west-1.amazonaws.com/other/marbaker.swe_black_young_man_wearing_japanese_desiner_shirt_hip_406f2134-bb19-4a61-8c99-82aabef0baa5+copy4.png",
+    "categories": [
+    "shirt",
+    "men"
+    ],
+    "size": [
+    "S",
+    "M",
+    "L"
+    ],
+    "color": [
+    "black",
+    "white",
+    "yellow",
+    "blue"
+    ],
+    "price": 31,
+    "inStock": true,
+    "createdAt": "2023-06-17T09:20:53.193Z",
+    "updatedAt": "2023-06-17T09:20:53.193Z",
+    "__v": 0
+    },
+    {
+    "_id": "648d851725e153e0de0e8511",
+    "title": "Hybrid Jeans With Vintage Patches",
+    "des": "designer jeans for 2022",
+    "img": "https://ecommercemcm.s3.us-west-1.amazonaws.com/Jeans/marbaker.swe_japanese_desiner_jeans_only_white_background_hip_h_11fbdee7-c088-4f64-9f5c-0830f84f15ba+copy1.png",
+    "categories": [
+    "jeans",
+    "men"
+    ],
+    "size": [
+    "S",
+    "M",
+    "L"
+    ],
+    "color": [
+    "black",
+    "white",
+    "yellow",
+    "blue"
+    ],
+    "price": 35,
+    "inStock": true,
+    "createdAt": "2023-06-17T10:04:07.668Z",
+    "updatedAt": "2023-06-17T10:04:07.668Z",
+    "__v": 0
+    },
+    {
+    "_id": "648d87b525e153e0de0e8513",
+    "title": "White Jeans With Horizontal Lines",
+    "des": "designer jeans for 2020",
+    "img": "https://ecommercemcm.s3.us-west-1.amazonaws.com/Jeans/marbaker.swe_japanese_desiner_jeans_only_white_background_hip_h_11fbdee7-c088-4f64-9f5c-0830f84f15ba+copy3.png",
+    "categories": [
+    "jeans",
+    "men"
+    ],
+    "size": [
+    "S",
+    "M",
+    "L"
+    ],
+    "color": [
+    "white",
+    "blue"
+    ],
+    "price": 39,
+    "inStock": true,
+    "createdAt": "2023-06-17T10:15:17.895Z",
+    "updatedAt": "2023-06-17T10:15:17.895Z",
+    "__v": 0
+    },
+    {
+    "_id": "648d882925e153e0de0e8519",
+    "title": "Light Jeans With Japanese Vibe Patches",
+    "des": "designer jeans for 2021",
+    "img": "https://ecommercemcm.s3.us-west-1.amazonaws.com/Jeans/marbaker.swe_japanese_desiner_jeans_only_white_background_hip_h_11fbdee7-c088-4f64-9f5c-0830f84f15ba+copy4.png",
+    "categories": [
+    "jeans",
+    "men"
+    ],
+    "size": [
+    "S",
+    "M",
+    "L"
+    ],
+    "color": [
+    "white",
+    "blue"
+    ],
+    "price": 43,
+    "inStock": true,
+    "createdAt": "2023-06-17T10:17:13.088Z",
+    "updatedAt": "2023-06-17T10:17:13.088Z",
+    "__v": 0
+    },
+    {
+    "_id": "648d88b325e153e0de0e851b",
+    "title": "Summer Vibe Jeans with Two Tone Colors",
+    "des": "designer jeans for 2021",
+    "img": "https://ecommercemcm.s3.us-west-1.amazonaws.com/Jeans/marbaker.swe_japanese_desiner_jeans_only_white_background_hip_h_ca2cf2ee-3574-4ea4-91fb-09e5330a0627+copy3.png",
+    "categories": [
+    "jeans",
+    "men"
+    ],
+    "size": [
+    "S",
+    "M",
+    "L"
+    ],
+    "color": [
+    "white",
+    "blue",
+    "black",
+    "red"
+    ],
+    "price": 50,
+    "inStock": true,
+    "createdAt": "2023-06-17T10:19:31.846Z",
+    "updatedAt": "2023-06-17T10:19:31.846Z",
+    "__v": 0
+    },
+    {
+    "_id": "648e14792cca1e83fa8a55af",
+    "title": "Kamikaze Japanese Shirt",
+    "des": "designer shirt for 2021",
+    "img": "https://ecommercemcm.s3.us-west-1.amazonaws.com/women/marbaker.swe_black_young_women_wearing_japanese_desiner_shirt_h_15690b40-1a7c-40e2-b4e4-90fb48812255+copy4.png",
+    "categories": [
+    "shirt",
+    "women"
+    ],
+    "size": [
+    "S",
+    "M",
+    "L"
+    ],
+    "color": [
+    "white",
+    "blue",
+    "black",
+    "red"
+    ],
+    "price": 25,
+    "inStock": true,
+    "createdAt": "2023-06-17T20:15:53.292Z",
+    "updatedAt": "2023-06-17T20:15:53.292Z",
+    "__v": 0
+    },
+    {
+    "_id": "648e14ab2cca1e83fa8a55b1",
+    "title": "Black and White Designer Shirt Japan",
+    "des": "designer shirt for 2020",
+    "img": "https://ecommercemcm.s3.us-west-1.amazonaws.com/women/marbaker.swe_black_young_women_wearing_japanese_desiner_shirt_h_15690b40-1a7c-40e2-b4e4-90fb48812255+copy5.png",
+    "categories": [
+    "shirt",
+    "women"
+    ],
+    "size": [
+    "S",
+    "M",
+    "L"
+    ],
+    "color": [
+    "white",
+    "blue",
+    "black",
+    "red"
+    ],
+    "price": 25,
+    "inStock": true,
+    "createdAt": "2023-06-17T20:16:43.475Z",
+    "updatedAt": "2023-06-17T20:16:43.475Z",
+    "__v": 0
+    },
+    {
+    "_id": "648e14c42cca1e83fa8a55b3",
+    "title": "Night Life Designer Shirt Japan",
+    "des": "designer shirt for 2022",
+    "img": "https://ecommercemcm.s3.us-west-1.amazonaws.com/women/marbaker.swe_black_young_women_wearing_japanese_desiner_shirt_h_c7eb5b5e-74a2-45d6-a284-6191b39babcb+copy1.png",
+    "categories": [
+    "shirt",
+    "women"
+    ],
+    "size": [
+    "S",
+    "M",
+    "L"
+    ],
+    "color": [
+    "white",
+    "blue",
+    "black",
+    "red"
+    ],
+    "price": 25,
+    "inStock": true,
+    "createdAt": "2023-06-17T20:17:08.840Z",
+    "updatedAt": "2023-06-17T20:17:08.840Z",
+    "__v": 0
+    },
+    {
+    "_id": "648e14df2cca1e83fa8a55b5",
+    "title": "Single Lady Designer Shirt Japan",
+    "des": "designer shirt for 2023",
+    "img": "https://ecommercemcm.s3.us-west-1.amazonaws.com/women/marbaker.swe_black_young_women_wearing_japanese_desiner_shirt_h_c7eb5b5e-74a2-45d6-a284-6191b39babcb+copy3.png",
+    "categories": [
+    "shirt",
+    "women"
+    ],
+    "size": [
+    "S",
+    "M",
+    "L"
+    ],
+    "color": [
+    "white",
+    "blue",
+    "black",
+    "red"
+    ],
+    "price": 25,
+    "inStock": true,
+    "createdAt": "2023-06-17T20:17:35.787Z",
+    "updatedAt": "2023-06-17T20:17:35.787Z",
+    "__v": 0
+    }
+    ]
